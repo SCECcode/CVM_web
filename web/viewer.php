0e3192e42c6c4ce2ee763718faf235c902494799
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-07-11 18:36:46 +0000

    update with newer vendor js

--- web/viewer.php
+++ web/viewer.php
@@ -38,7 +38,7 @@ $header = getHeader("Viewer");
 
     <script type='text/javascript' src='js/vendor/FileSaver.js'></script>
     <script type='text/javascript' src='js/vendor/jszip.js'></script>
-
+    <script type='text/javascript' src='js/vendor/togeojson.js'></script>
     <script type='text/javascript' src='js/vendor/leaflet-kmz-src.js'></script>
     <script type='text/javascript' src='js/vendor/leaflet.markercluster-src.js'></script>
 
