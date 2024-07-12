81adc1f3ee10ef9be7b60b717a9a55d803ab9c62
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2024-07-12 06:51:30 +0000

    wip

---
+++ web/js/cvm.js
@@ -0,0 +1,53 @@
+/***
+   cvm.js
+***/
+
+var CVM = new function () {
+
+  this.model_debug = 0;
+  this.mdoel_initialized = false;
+
+// gather up the valid cvm models for this instance
+  this.setup_model = function() {
+    getInstallModelList();
+  };
+
+  this.setupCVMInterface = function() {
+
+// setup various info tables
+// parameter table 
+     document.getElementById('parametersTable-container').innerHTML=makeParametersTable();
+// file format table
+     document.getElementById('fileFormatTable-container').innerHTML=makeFileFormatTable();
+// zmode table
+     document.getElementById('ZModeTable-container').innerHTML=makeZModeTable();
+
+//??? starting mode, first model one list, option for point, option for depth
+	  //
+   };
+
+/******************************************/
+    this.processByLatlonForPoint = function(fromMap) {
+      document.getElementById('spinIconForProperty').style.display = "block";
+      getMaterialPropertyByLatlon();
+    };
+ 
+    this.processByLatlonForProfile = function(fromMap) {
+      document.getElementById('spinIconForProfile').style.display = "block";
+      plotVerticalProfile();
+    };
+
+    this.processByLatlonForLine = function(fromMap) {
+      document.getElementById('spinIconForLine').style.display = "block";
+      plotCrossSection();
+    };
+
+    this.processByLatlonForArea = function(fromMap) {
+      document.getElementById('spinIconForArea').style.display = "block";
+      plotHorizontalSlice();
+    };
+
+};
+
+
+
